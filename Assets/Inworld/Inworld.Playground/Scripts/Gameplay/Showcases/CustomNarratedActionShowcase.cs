b33eb12a55f11a447adad75b3a3c986f7cfcf425
Matt Wilson <matt.wilson720@gmail.com>
2024-04-09 10:15:25 -0700

    Added Custom Narrated Action Showcase script

---
+++ Assets/Inworld/Inworld.Playground/Scripts/Gameplay/Showcases/CustomNarratedActionShowcase.cs
@@ -0,0 +1,32 @@
+/*************************************************************************************************
+ * Copyright 2024 Theai, Inc. dba Inworld AI
+ *
+ * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+ * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+ *************************************************************************************************/
+
+using TMPro;
+using UnityEngine;
+
+namespace Inworld.Playground
+{
+    /// <summary>
+    ///     Handles the Custom Narrated Action showcase which allows a custom narrated action input
+    ///      by the player to be sent to an Inworld character.
+    /// </summary>
+    public class CustomNarratedActionShowcase : MonoBehaviour
+    {
+        [SerializeField] protected InworldCharacter m_InworldCharacter;
+        [SerializeField] protected TMP_InputField m_InputField;
+        
+        /// <summary>
+        ///     Sends a narrated action to m_InworldCharacter using the input from m_InputField.
+        /// </summary>
+        public void SendNarratedAction()
+        {
+            if (string.IsNullOrEmpty(m_InputField.text)) return;
+            
+            m_InworldCharacter.SendNarrative(m_InputField.text);
+        }
+    }
+}
