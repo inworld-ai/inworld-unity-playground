9e706883a460149b95981bfff59de904caec8464
Matt Wilson <matt.wilson720@gmail.com>
2024-05-07 17:51:16 -0700

    Updated Scene room with unitary session showcases, fixed issue with SightAngle, added Angel and Devil Innequin models

---
+++ Assets/Inworld/Inworld.Playground/Scripts/Gameplay/Showcases/PlayerNameShowcase.cs
@@ -0,0 +1,26 @@
+/*************************************************************************************************
+ * Copyright 2024 Theai, Inc. dba Inworld AI
+ *
+ * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+ * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+ *************************************************************************************************/
+
+using TMPro;
+using UnityEngine;
+
+namespace Inworld.Playground
+{
+    /// <summary>
+    ///     Handler for the Update Player Name showcase.
+    /// </summary>
+    public class PlayerNameShowcase : InputShowcase
+    {
+        public void SendPlayerNameChangeRequest()
+        {
+            if (PlaygroundManager.Instance.GetPlayerName() == m_InputField.text)
+                return;
+            PlaygroundManager.Instance.SetPlayerName(m_InputField.text);
+            InworldController.Client.SendUserConfig();
+        }
+    }
+}
