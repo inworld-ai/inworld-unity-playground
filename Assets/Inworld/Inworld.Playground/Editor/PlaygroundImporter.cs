c6e17b54f485d88eb1832aaba264e624b20046f7
Yan Jin <yan.jin@inworld.ai>
2024-12-12 16:56:48 -0800

    INTG-2137 add dependancy to inworld-unity-full

---
+++ Assets/Inworld/Inworld.Playground/Editor/PlaygroundImporter.cs
@@ -0,0 +1,80 @@
+﻿/*************************************************************************************************
+ * Copyright 2022-2024 Theai, Inc. dba Inworld AI
+ *
+ * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+ * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+ *************************************************************************************************/
+
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+
+namespace Inworld.Playground
+{
+    [InitializeOnLoad]
+    public class PlaygroundImporter : AssetPostprocessor
+    {
+        const string k_LegacyPkgName = "Inworld.AI";
+        const string k_InworldPath = "Assets/Inworld";
+        const string k_PlaygroundPath = "Inworld.Playground";
+        const string k_ErrorTitle = "Install Playground Failed";
+        //TODO(Yan): Replace to inworld-unity-core package once Playground is fully detached by full package.
+        //           For the UserData Checking, let's add once EditorApplication starts playing.
+        const string k_NoSDKDescription = "Inworld SDK is not found\nPlease download and install the InworldAI.Full.unitypackage first.\nThen select Inworld > Playground > Reinstall.";
+        const string k_UpgradeContent = "Legacy Inworld SDK found.\nPlease delete the folder Assets/Inworld, and import the newest Inworld Unity SDK package\nThen select Inworld > Playground > Reinstall.";
+        const string k_MissingPackage = "Missing playground unitypackage.\nPlease download the InworldPlayground.unitypackage and reimport.";
+        const string k_PkgName = "InworldPlayground";
+        const string k_AssetPkgName = "InworldPlaygroundAssets";
+        const string k_DependencyPackage = "com.inworld.unity.core";
+        
+        static PlaygroundImporter()
+        {
+            AssetDatabase.importPackageCompleted += name =>
+            {
+                if (name == k_PkgName)
+                    CheckDependencies();
+                if (name == k_AssetPkgName)
+                    Debug.Log($"{k_PkgName} installed.");
+            };
+        }
+        [MenuItem("Inworld/Playground/Reinstall")]
+        public static async void CheckDependencies()
+        {
+            Debug.Log("Checking Playground Dependencies...");
+            if (Directory.Exists($"Assets/{k_LegacyPkgName}") || Directory.Exists($"{k_InworldPath}/{k_LegacyPkgName}"))
+            {
+                EditorUtility.DisplayDialog(k_ErrorTitle, k_UpgradeContent, "OK");
+                return;
+            } 
+            ListRequest listRequest = UnityEditor.PackageManager.Client.List();
+
+            while (!listRequest.IsCompleted)
+            {
+                await Task.Yield();
+            }
+            if (listRequest.Status != StatusCode.Success)
+            {
+                Debug.LogError(listRequest.Error.ToString());
+                return;
+            }
+            if (listRequest.Result.All(x => x.name != k_DependencyPackage))
+            {
+                EditorUtility.DisplayDialog(k_ErrorTitle, k_NoSDKDescription, "OK");
+                return;
+            }
+            if (Directory.Exists($"{k_InworldPath}/{k_PlaygroundPath}/Runtime"))
+                return;
+            if (!File.Exists($"{k_InworldPath}/{k_PlaygroundPath}/{k_AssetPkgName}.unitypackage"))
+            {
+                EditorUtility.DisplayDialog(k_ErrorTitle, k_MissingPackage, "OK");
+                return;
+            }
+            Debug.Log("Import Playground Dependency Packages...");
+            AssetDatabase.ImportPackage($"{k_InworldPath}/{k_PlaygroundPath}/{k_AssetPkgName}.unitypackage", false);
+        }
+    }
+}
